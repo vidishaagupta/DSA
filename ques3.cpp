711aa8b28bf84ad124caa4422e60c6e840b26648
vidishaa <152952157+vidishaagupta@users.noreply.github.com>
2026-07-09 11:23:19 +0530

    Update ques3.cpp

--- ques3.cpp
+++ ques3.cpp
@@ -65,3 +65,71 @@ public:
         return image;
     }
 };
+
+//Search a 2D Matrix
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int l=0;
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        int h=n*m-1;
+
+        while(l<=h){
+            int mid=(l+h)/2;
+            int r=mid/m;
+            int c=mid%m;
+
+            if(matrix[r][c]==target){
+                return true;
+            }else if(matrix[r][c]<target){
+                l=mid+1;
+            }else{
+                h=mid-1;
+            }
+        }
+        return false;
+    }
+};
+
+//Set Matrix Zeroes
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
+        int col0=1;
+        int n=matrix.size();
+        int m=matrix[0].size();
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(matrix[i][j]==0){
+                    matrix[i][0]=0;
+
+                    if(j!=0){
+                        matrix[0][j]=0;
+                    }else{
+                        col0=0;
+                    }
+                }
+            }
+        }
+
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                if(matrix[i][j]!=0){
+                    if(matrix[0][j]==0 || matrix[i][0]==0){
+                        matrix[i][j]=0;
+                    }
+                }
+            }
+        }
+
+        if(matrix[0][0]==0){
+            for(int j=0;j<m;j++) matrix[0][j]=0;
+        }
+        if(col0==0){
+            for(int i=0;i<n;i++) matrix[i][0]=0;
+        }
+    }
+};
