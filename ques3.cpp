05cfb04f6cf3ab4078a7c31c01fbf7517835122c
vidishaa <152952157+vidishaagupta@users.noreply.github.com>
2026-07-09 11:57:08 +0530

    Update ques3.cpp

--- ques3.cpp
+++ ques3.cpp
@@ -179,4 +179,53 @@ public:
     }
 };
 
+//Reorder list
+class Solution {
+public:
+    void reorderList(ListNode* head) {
 
+        if(head == NULL || head->next == NULL)
+            return;
+
+
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while(fast->next != NULL && fast->next->next != NULL){
+            slow = slow->next;
+            fast = fast->next->next;
+        }
+
+
+        ListNode* curr = slow->next;
+        slow->next = NULL;
+
+        ListNode* prev = NULL;
+
+        while(curr != NULL){
+
+            ListNode* nextNode = curr->next;
+
+            curr->next = prev;
+
+            prev = curr;
+            curr = nextNode;
+        }
+
+
+        ListNode* first = head;
+        ListNode* second = prev;
+
+        while(second != NULL){
+
+            ListNode* temp1 = first->next;
+            ListNode* temp2 = second->next;
+
+            first->next = second;
+            second->next = temp1;
+
+            first = temp1;
+            second = temp2;
+        }
+    }
+};
