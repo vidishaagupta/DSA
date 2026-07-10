dc8869d001978d82eb0a579da1569fb5006067dd
vidishaa <152952157+vidishaagupta@users.noreply.github.com>
2026-07-10 15:32:55 +0530

    Update ques3.cpp

--- ques3.cpp
+++ ques3.cpp
@@ -337,3 +337,39 @@ public:
     }
 };
 
+//Remove K Digits
+class Solution {
+public:
+    string removeKdigits(string s, int k) {
+        int n=s.size();
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            while(!st.empty() && k>0 && (st.top()-'0')> (s[i]-'0')){
+                st.pop();
+                k=k-1;
+            }
+            st.push(s[i]);
+        }
+
+        while(k>0){
+            st.pop();
+            k--;
+        }
+
+        if(st.empty()) return "0";
+
+        string res="";
+        while(!st.empty()){
+            res+=st.top();
+            st.pop();
+        }
+
+        while(res.size()!=0 && res.back()=='0'){
+            res.pop_back();
+        }
+        reverse(res.begin(), res.end());
+        if(res.empty()) return "0";
+        return res;
+    }
+};
+
